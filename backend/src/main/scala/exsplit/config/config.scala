2944767e87c46c9762de8f2722af8f4f8bceffa3
ChidiRNweke <chidi125@gmail.com>
2024-03-16 13:11:42 +0100

    Add configReader helper

--- backend/src/main/scala/exsplit/config/config.scala
+++ backend/src/main/scala/exsplit/config/config.scala
@@ -2,6 +2,7 @@ package exsplit.config
 import cats.Functor
 import pureconfig._
 import pureconfig.generic.derivation.default._
+import scala.reflect.ClassTag
 
 trait AuthConfig[F[_]: Functor]:
   val secretKey: F[String]
@@ -19,3 +20,8 @@ case class DatabaseConfig(
     migrationsTable: String,
     migrationsLocations: List[String]
 ) derives ConfigReader
+
+def configToClassFromResource[T](
+    resourcePath: String
+)(using ConfigReader[T], ClassTag[T]): T =
+  ConfigSource.resources(resourcePath).loadOrThrow[T]
