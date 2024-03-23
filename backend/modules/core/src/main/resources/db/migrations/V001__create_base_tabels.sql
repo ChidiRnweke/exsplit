1c91b2ecf66c2dbb054b79255943915733fe6282
ChidiRNweke <chidi125@gmail.com>
2024-03-23 14:14:30 +0100

    Update base tables

--- backend/modules/core/src/main/resources/db/migrations/V001__create_base_tabels.sql
+++ backend/modules/core/src/main/resources/db/migrations/V001__create_base_tabels.sql
@@ -17,32 +17,49 @@ create table circles (
 );
 
 create table circle_members (
+    id text primary key default md5(now()::text || random()::text),
     display_id serial primary key,
     user_id text,
     circle_id text,
-    foreign key (user_id) references "user"(id) on delete cascade,
-    foreign key (circle_id) references circle(id) on delete cascade
+    foreign key (user_id) references "users"(id) on delete cascade,
+    foreign key (circle_id) references circle(id) on delete cascade,
+    unique (user_id)
+);
+
+create table settled_tabs (
+    id text primary key default md5(now()::text || random()::text),
+    circle_id text not null references circle(id),
+    from_member text not null references circle_members(id),
+    to_member text not null references circle_members(id),
+    amount float not null,
+    settled_at date not null default current_date
 );
 
 create table expense_lists (
     id text primary key default md5(now()::text || random()::text),
     name varchar(255) not null,
-    circle_id text not null references circle(id)
+    circle_id text not null references circle(id),
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
 );
 
 create table expenses (
     id text primary key default md5(now()::text || random()::text),
     expense_list_id text not null references expense_list(id),
-    initial_payer varchar(255) not null references "user"(id),
+    paid_by varchar(255) not null references circle_members(id),
     description text not null,
     price float not null,
-    date date not null
+    date date not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
 );
 
-create table owed_expenses (
+create table owed_amounts (
     id text primary key default md5(now()::text || random()::text),
     expense_id text not null references expense(id),
-    person_to varchar(255) not null references "user"(id),
-    person_from varchar(255) not null references "user"(id),
-    amount float not null
+    from_member varchar(255) not null references circle_members(id),
+    to_member varchar(255) not null references circle_members(id),
+    amount float not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
 );
