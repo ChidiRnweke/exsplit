fe001f42184f0786845c89cd49ee626618afd3a2
ChidiRNweke <chidi125@gmail.com>
2024-04-01 23:13:25 +0200

    Move repository to config

--- backend/modules/core/src/main/scala/exsplit/config.scala
+++ backend/modules/core/src/main/scala/exsplit/config.scala
@@ -4,6 +4,19 @@ import cats.Functor
 import pureconfig._
 import pureconfig.generic.derivation.default._
 import scala.reflect.ClassTag
+import cats.effect._
+import cats.syntax.all._
+import cats.data._
+import cats._
+import exsplit.config._
+import skunk._
+import skunk.implicits._
+import skunk.codec.all._
+import natchez.Trace.Implicits.noop
+import fs2.io.net.Network
+import natchez._
+import scala.annotation.targetName
+import fs2._
 
 trait AuthConfig[F[_]: Functor]:
   val secretKey: F[String]
@@ -28,3 +41,15 @@ def configToClassFromResource[T](
     resourcePath: String
 )(using ConfigReader[T], ClassTag[T]): T =
   ConfigSource.resources(resourcePath).loadOrThrow[T]
+
+object SessionPool:
+  def makePool[F[_]: Temporal: natchez.Trace: std.Console: Network](
+      repositoryConfig: RepositoryConfig
+  ): Resource[F, Resource[F, Session[F]]] =
+    Session.pooled[F](
+      host = repositoryConfig.host,
+      user = repositoryConfig.user,
+      password = Some(repositoryConfig.password),
+      database = repositoryConfig.database,
+      max = repositoryConfig.max
+    )
