1e5563d5c21bfae6a0406e96d47eacbfcb571241
ChidiRNweke <chidi125@gmail.com>
2024-04-03 20:47:00 +0200

    Add UserRepository tests for CRUD operations

---
+++ backend/modules/integration/src/test/scala/datamapper/UserRepository.test.scala
@@ -0,0 +1,67 @@
+import cats.effect._
+import cats.syntax.all._
+import cats.effect.std.UUIDGen
+import cats.data._
+import cats._
+import exsplit.config._
+import exsplit.integration._
+import exsplit.spec._
+import exsplit.datamapper.circles._
+import exsplit.spec.CirclesServiceOperation.reified.createCircle
+import exsplit.spec.UserId
+import exsplit.datamapper.user._
+
+class UserRepositorySuite extends DatabaseSuite:
+  test("You should be able to create a user in the database"):
+    val session = sessionPool()
+    session.use(session =>
+      for
+        uuid <- UUIDGen[IO].randomUUID
+        userRepo <- UserMapper.fromSession(session)
+        result <- userRepo
+          .createUser(uuid, Email("user@foo.com"), "password")
+          .attempt
+      yield assertEquals(result.isRight, true)
+    )
+
+  test("You should be able to read a user from the database"):
+    val session = sessionPool()
+    session.use(session =>
+      for
+        uuid <- UUIDGen[IO].randomUUID
+        userRepo <- UserMapper.fromSession(session)
+        _ <- userRepo.createUser(uuid, Email("user@foo.com"), "password")
+        obtained <- userRepo.get(uuid.toString()).rethrow
+        expected = UserReadMapper(uuid.toString(), "user@foo.com", "password")
+      yield assertEquals(obtained, expected)
+    )
+
+  test("You should be able to delete a user from the database"):
+    val session = sessionPool()
+    session.use(session =>
+      for
+        uuid <- UUIDGen[IO].randomUUID
+        userRepo <- UserMapper.fromSession(session)
+        _ <- userRepo.createUser(uuid, Email("foo@foo.com"), "password")
+        _ <- userRepo.delete(uuid.toString())
+        obtained <- userRepo.get(uuid.toString()) // The user should not exist
+      yield assertEquals(obtained.isLeft, true)
+    )
+
+  test("You should be able to update a user in the database"):
+    val session = sessionPool()
+    session.use(session =>
+      for
+        uuid <- UUIDGen[IO].randomUUID
+        userRepo <- UserMapper.fromSession(session)
+        _ <- userRepo.createUser(uuid, Email("foo@foo.com"), "password")
+        userWrite = UserWriteMapper(
+          uuid.toString(),
+          Some("foo@bar.com"),
+          Some("newpassword")
+        )
+        _ <- userRepo.update(userWrite)
+        obtained <- userRepo.get(uuid.toString()).rethrow
+        expected = UserReadMapper(uuid.toString(), "foo@bar.com", "newpassword")
+      yield assertEquals(obtained, expected)
+    )
