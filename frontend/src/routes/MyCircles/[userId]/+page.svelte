e1377a6ff61e98bf09455e747f260c868f336b22
ChidiRNweke <chidi125@gmail.com>
2024-04-27 18:33:49 +0200

    Fix MyCircles dummy page by disabling SSR so load works properly

--- frontend/src/routes/MyCircles/[userId]/+page.svelte
+++ frontend/src/routes/MyCircles/[userId]/+page.svelte
@@ -1,3 +1,24 @@
 <script lang="ts">
-
+	import type { PageData } from './$types';
+	import Card from 'flowbite-svelte/Card.svelte';
+	import Button from 'flowbite-svelte/Button.svelte';
+	import Paragraph from '../../../components/shared/Paragraph.svelte';
+	export let data: PageData;
+	let numCircles = data.circles.length;
 </script>
+
+{#if numCircles === 0}
+	<h3>You don't have any circles yet. You can make them here.</h3>
+	<Button />
+{:else}
+	{#each data.circles as circle}
+		<Card>
+			<h5>
+				{circle.circleName}
+			</h5>
+			<Paragraph>
+				{circle.description}
+			</Paragraph>
+		</Card>
+	{/each}
+{/if}
