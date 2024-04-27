809e03ab4eb017ebf85d169f3c87502184205faf
ChidiRNweke <chidi125@gmail.com>
2024-04-27 19:17:06 +0200

    Initialize app correctly upon startup

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,4 +1,6 @@
 import { writable } from 'svelte/store';
+import { userClient } from './api/clients/UserService';
+import { goto } from '$app/navigation';
 
 export const loggedInStatusStore = writable(false);
 export const userIdStore = writable<string | null>(null);
@@ -8,7 +10,29 @@ export const storeLoginData = (userId: string) => {
 	userIdStore.set(userId);
 };
 
-export const handleLogout = () => {
+export const logout = () => {
 	loggedInStatusStore.set(false);
 	userIdStore.set(null);
+	localStorage.removeItem('accessToken');
+	localStorage.removeItem('refreshToken');
+	localStorage.removeItem('userId');
+};
+
+export const initialState = async () => {
+	const maybeUser = localStorage.getItem('userId');
+	const refreshToken = localStorage.getItem('refreshToken');
+
+	if (maybeUser && refreshToken) {
+		const { error } = await userClient.refresh({ refreshToken });
+		const user = localStorage.getItem('userId');
+		// The user client takes care of storing the new access token as well as the user id
+		// so we only need to set the user id here
+		userIdStore.set(user);
+		if (error) {
+			logout();
+			goto('login');
+		} else {
+			loggedInStatusStore.set(true);
+		}
+	}
 };
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,12 +3,20 @@
 	import Footer from '../components/shared/Footer.svelte';
 	import Main from '../components/shared/Main.svelte';
 	import Nav from '../components/shared/Nav.svelte';
+	import { onMount } from 'svelte';
+	import { initialState } from '$lib/stores';
+
+	onMount(async () => await initialState());
 </script>
 
-<Nav class="sticky top-0 z-50 mb-10 border-b-2" />
+<div
+	class="dark:dark-bg-background bg-background text-foreground dark:bg-dark-background dark:text-dark-foreground"
+>
+	<Nav class="sticky top-0 z-50 mb-10 border-b-2" />
 
-<Main>
-	<slot />
-</Main>
+	<Main>
+		<slot />
+	</Main>
 
-<Footer class="mb-10 ml-5 mr-5 mt-10 text-center lg:ml-56 lg:mr-56" />
+	<Footer class="mb-10 ml-5 mr-5 mt-10 text-center lg:ml-56 lg:mr-56" />
+</div>
