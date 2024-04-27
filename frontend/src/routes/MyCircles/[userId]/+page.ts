5f8c543baed9a8d4f52dbdbc39b60d5c3b39fef7
ChidiRNweke <chidi125@gmail.com>
2024-04-27 18:30:41 +0200

    Return circles object instead of raw array in MyCircles load
    Kit does not allow you to return raw arrays, they need to be within an object.

--- frontend/src/routes/MyCircles/[userId]/+page.ts
+++ frontend/src/routes/MyCircles/[userId]/+page.ts
@@ -1,9 +1,9 @@
 import { circlesClient } from '$lib/api/clients/CirclesService';
-import type { Circle } from '$lib/types/circles';
+import type { UserForCircleOutput } from '$lib/api/types/circles';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params }): Promise<Circle[]> => {
+export const load: PageLoad = async ({ params }): Promise<UserForCircleOutput> => {
 	const userId = params.userId;
 	const circles = await circlesClient.listCirclesForUser({ userId });
-	return circles.circles;
+	return circles;
 };
